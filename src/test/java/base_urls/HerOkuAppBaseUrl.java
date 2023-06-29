6891e8511c01407f18647fd54ee08c14765ad2f2
Emre Ozer <constellation2023@gmail.com>
2023-06-29 18:10:52 +0300

    Post03

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -1,6 +1,7 @@
 package base_urls;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
@@ -12,7 +13,7 @@ public class HerOkuAppBaseUrl {
     @Before //This annotation runs the method before each @Test method. Then spec object will be assigned.
     public void setUp(){
 
-        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
 
 
     }
