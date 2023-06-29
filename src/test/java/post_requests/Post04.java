e2ff7dbaf838b6cb6daa37a0c8955f4cf6207b8f
Emre Ozer <constellation2023@gmail.com>
2023-06-29 18:34:08 +0300

    Post04

---
+++ src/test/java/post_requests/Post04.java
@@ -0,0 +1,51 @@
+package post_requests;
+
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+import static io.restassured.RestAssured.given;
+
+public class Post04 extends JsonPlaceHolderBaseUrl {
+
+       /*
+         Given
+            https://jsonplaceholder.typicode.com/todos
+            {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+            }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void post04(){
+        //Set the url
+        spec.pathParam("first","todos");
+
+        //Set the expected data --> We will set th expected data sa Pojo class
+        JsonPlaceHolderPojo expectedData = new JsonPlaceHolderPojo(55,"Tidy your room",false);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response = given(spec).body(expectedData).post("{first}");
+        response.prettyPrint();
+
+        //Do assertion
+
+
+
+    }
+}
