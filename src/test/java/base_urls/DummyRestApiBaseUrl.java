6548335a1da991f874668b67237365cfda3a590c
Emre Ozer <constellation2023@gmail.com>
2023-07-03 16:10:57 +0300

    3/7/2023

--- src/test/java/base_urls/DummyRestApiBaseUrl.java
+++ src/test/java/base_urls/DummyRestApiBaseUrl.java
@@ -1,10 +1,11 @@
 package base_urls;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-public class PetStoreBaseUrl {
+public class DummyRestApiBaseUrl {
     //This class is created to prevent repeated actions in requests like baser url, content type, authorization, etc.
 
     protected RequestSpecification spec;
@@ -12,8 +13,6 @@ public class PetStoreBaseUrl {
     @Before //This annotation runs the method before each @Test method. Then spec object will be assigned.
     public void setUp(){
 
-        spec = new RequestSpecBuilder().setBaseUri("https://petstore.swagger.io/v2").build();
-
-
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://dummy.restapiexample.com/api/v1").build();
     }
 }
