d965ae54e21d6caecdc7f8200f36f9e8ba64025a
Emre Ozer <constellation2023@gmail.com>
2023-07-08 19:51:46 +0300

    8/7/2023

--- src/test/java/petstore_smoketest/C05_GetPet_Negative.java
+++ src/test/java/petstore_smoketest/C05_GetPet_Negative.java
@@ -1,7 +1,12 @@
 package petstore_smoketest;
 
 import base_urls.PetStoreBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertEquals;
+import static petstore_smoketest.C01_CreatePet.petId;
 
 public class C05_GetPet_Negative extends PetStoreBaseUrl {
     /*
@@ -20,12 +25,33 @@ public class C05_GetPet_Negative extends PetStoreBaseUrl {
         }
      */
 
+    @Test
+    public void get02(){
+        //Set the url
+        spec.pathParams("first","pet","second", petId);
 
+        //Set the expected data
+        String expectedData = "Pet not found";
 
+        //Send the request and get the response
+        Response response = given(spec).get("{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+        String actualData = response.jsonPath().getString("message");
+
+        assertEquals(404, response.statusCode());
+        assertEquals(expectedData, actualData);
 
 
     }
 }
+
+
+
+
+
+
 
 
 
