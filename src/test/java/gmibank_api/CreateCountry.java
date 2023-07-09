ac49f74251d99fcac624567ffa814f375e0adb81
Emre Ozer <constellation2023@gmail.com>
2023-07-09 19:53:03 +0300

    9/7/2023

--- src/test/java/gmibank_api/CreateCountry.java
+++ src/test/java/gmibank_api/CreateCountry.java
@@ -1,6 +1,7 @@
 package gmibank_api;
 
 import base_urls.GmiBankBaseUrl;
+import com.google.gson.Gson;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
@@ -65,13 +66,13 @@ public void post01(){
 
     CountryPost expectedData = new CountryPost("Banana Republic", stateList);
     System.out.println("expectedData = " + expectedData);
-    
+
     //Send the request and get the response
     Response response = given(spec).body(expectedData).post("{first}/{second}");
     response.prettyPrint();
 
     //Do assertion
-    //1st Validation
+    //1st Validation: response.then() method
     response
             .then()
             .statusCode(201)
@@ -84,16 +85,16 @@ public void post01(){
                     "states.name[2]", equalTo(state3.getName())
             );
 
-    //2nd Validation
+    //2nd Validation: jsonPath()
     JsonPath jsonPath = response.jsonPath();
 
     assertEquals(201, response.statusCode());
     assertEquals(expectedData.getName(), jsonPath.getString("name"));
-    
+
     assertEquals(state1.getId(), jsonPath.getList("states.id").get(0));
     assertEquals(state1.getName(), jsonPath.getList("states.name").get(0));
 
-    //3rd Validation
+    //3rd Validation: converting to as() method
     Map<String, Object> actualDataMap =  response.as(HashMap.class);
     System.out.println("actualDataMap = " + actualDataMap);
 
@@ -102,8 +103,19 @@ public void post01(){
     assertEquals(state1.getId(), ((Map)((List)actualDataMap.get("states")).get(0)).get("id"));
     assertEquals(state1.getName(), ((Map)((List)actualDataMap.get("states")).get(0)).get("name"));
 
-    //4th Validation
+    //4th Validation: Pojo Class - converting to as() method
+    CountryPost actualDataPojo=response.as(CountryPost.class);
+    System.out.println("actualDataPojo = " + actualDataPojo);
 
+    assertEquals(expectedData.getName(),actualDataPojo.getName());
+    assertEquals(state1.getId(),actualDataPojo.getStates().get(0).getId());
+    assertEquals(state1.getName(),actualDataPojo.getStates().get(0).getName());
+
+    assertEquals(state2.getId(),actualDataPojo.getStates().get(1).getId());
+    assertEquals(state2.getName(),actualDataPojo.getStates().get(1).getName());
+
+    assertEquals(state3.getId(),actualDataPojo.getStates().get(2).getId());
+    assertEquals(state3.getName(),actualDataPojo.getStates().get(2).getName());
 
     //5th Validation --> Best practice: Pojo Class + Object Mapper
     CountryPost actualDataPojoMapper = convertJsonToJava(response.asString(), CountryPost.class);
@@ -120,6 +132,21 @@ public void post01(){
     assertEquals(state3.getId(), actualDataPojoMapper.getStates().get(2).getId());
     assertEquals(state3.getName(), actualDataPojoMapper.getStates().get(2).getName());
 
+    //6th Validation: Gson().fromJson() method
+    CountryPost actualDataPojoGson = new Gson().fromJson(response.asString(), CountryPost.class);
+    System.out.println("actualDataPojoGson = " + actualDataPojoGson);
 
-    }
+    assertEquals(expectedData.getName(), actualDataPojoGson.getName());
+
+    assertEquals(state1.getId(), actualDataPojoGson.getStates().get(0).getId());
+    assertEquals(state1.getName(), actualDataPojoGson.getStates().get(0).getName());
+
+    assertEquals(state2.getId(), actualDataPojoGson.getStates().get(1).getId());
+    assertEquals(state2.getName(), actualDataPojoGson.getStates().get(1).getName());
+
+    assertEquals(state3.getId(), actualDataPojoGson.getStates().get(2).getId());
+    assertEquals(state3.getName(), actualDataPojoGson.getStates().get(2).getName());
+
+
+}
 }
